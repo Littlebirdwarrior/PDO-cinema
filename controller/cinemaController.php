0470364e7b3ec81ad7f3215ccfda686e96f9aada
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-03-20 23:15:28 +0100

    organisation des fichier, session, recuperation requete k.o

---
+++ controller/cinemaController.php
@@ -0,0 +1,49 @@
+<?php
+//un seul fichier pour le moment également qui contiendra l'ensemble des requêtes dans les fonctions en relation avec les vues 
+
+namespace Controller;
+use Model\Connect;
+
+
+class CinemaController {
+    //lister les film
+    public function listFilms(){
+        $pdo = Connect::seConnecter();
+
+        $requete = $pdo->query('
+        SELECT
+                film.titre_film,
+                film.annee_sortie_film,
+                TIME_FORMAT(SEC_TO_TIME(film.duree_film * 60), "%H:%i") AS duree_film,
+                personne.prenom_personne,
+                personne.nom_personne
+            FROM
+            film
+                INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+                INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+    ');
+    require "view/listFilms.php";
+    }
+}
+
+
+
+
+
+//use Controller\CinemaController;
+//
+////On autocharge les classes du projet 
+//spl_autoload_register(function($class_name){
+//    include $class_name . 'php';
+//});
+//
+//$ctrlCinema = new CinemaController();
+//
+//if(isset($GET["action"])){
+//    switch ($_GET["action"]){
+//        case "listFilms" : $ctrlCinema->listFilms(); break;
+//        case "listActeurs" : $ctrlCinema->listActeurs(); break;
+//    }
+//}
+
+
---
+++ model/connect.php
@@ -0,0 +1,23 @@
+
+<?php
+//Dans le fichier "Connect.php" on se contente de déclarer la connexion à la base de données 
+namespace Model;
+
+abstract class Connect {
+    const HOST="localhost";
+    const DB = "cinema";
+    const USER = "root";
+    const PASS ="";
+
+//La classe est abstraite, on n'instanciera jamais la classe Connect puisqu'on aura seulement besoin d'accéder à la méthode "seConnecter"
+    public static function seConnecter(){
+        try {
+            return new \PDO(
+                "mysql:host".self::HOST."dbname=".self::DB.";charset=utf8", self::USER, self::PASS);
+        }catch(\PDOException $ex){
+            return $ex->getMessage();
+        }
+    }
+
+}
+
