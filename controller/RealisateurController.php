f22e882f1ed18ab2e755cc2f3343cfc78e243e84
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-03-24 11:53:13 +0100

    $requetelistRealisateur correctement nommée

--- controller/RealisateurController.php
+++ controller/RealisateurController.php
@@ -10,7 +10,7 @@ class RealisteurController
      //Lister les Réalisateurs
      public function listRealisateurs(){
         $pdo = Connect::seConnecter();
-        $requeteDetailRealisateur = $pdo->query("
+        $requeteListRealisateur = $pdo->query("
         SELECT
             CONCAT(personne.prenom_personne, ' ', personne.nom_personne) AS realisateurs,
             GROUP_CONCAT(CONCAT_WS('-', film.titre_film, film.annee_sortie_film) SEPARATOR ' | ') AS films
@@ -25,24 +25,24 @@ class RealisteurController
         require "view/listRealisateurs.php";
      }
 
-    public function detailRealisateur($id)
-    {
-    //Identité d'un réalisateurs
-    $pdo = Connect::seConnecter();
-    $requeteDetailReal = $pdo->prepare("
-    SELECT
-        CONCAT(personne.prenom_personne, ' ', personne.nom_personne) AS realisateurs,
-        CONCAT(film.titre_film, ' (', film.annee_sortie_film, ')') AS films
-    FROM
-        film
-        INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
-        INNER JOIN personne ON realisateur.id_personne = personne.id_personne
-    WHERE
-        realisateur.id_realisateur = :id
-    ");
-    $requeteDetailReal -> execute(["id" => $id]);
-    require "view/detailReal.php";
-    }
+    // public function detailRealisateur($id)
+    // {
+    // //Identité d'un réalisateurs
+    // $pdo = Connect::seConnecter();
+    // $requeteDetailReal = $pdo->prepare("
+    // SELECT
+    //     CONCAT(personne.prenom_personne, ' ', personne.nom_personne) AS realisateurs,
+    //     CONCAT(film.titre_film, ' (', film.annee_sortie_film, ')') AS films
+    // FROM
+    //     film
+    //     INNER JOIN realisateur ON film.id_realisateur = realisateur.id_realisateur
+    //     INNER JOIN personne ON realisateur.id_personne = personne.id_personne
+    // WHERE
+    //     realisateur.id_realisateur = :id
+    // ");
+    // $requeteDetailReal -> execute(["id" => $id]);
+    // require "view/detailReal.php";
+    // }
 
 
 }
