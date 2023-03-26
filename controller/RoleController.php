21db3684a9933ce1c09fd5b7d850a54daf3d0a01
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-03-26 03:00:24 +0200

    requete sql detailRole ok

--- controller/RoleController.php
+++ controller/RoleController.php
@@ -28,5 +28,29 @@ class RoleController
             require "view/listRoles.php";
     }
 
+    public function detailRole($id)
+    {
+        //Afficher les informations sur le film (id dans url)
+        $pdo = Connect::seConnecter();
+        $requeteDetailRole = $pdo->prepare("
+        SELECT
+            r.id_role,
+            r.nom_role,
+            a.id_acteur,
+            CONCAT(p.prenom_personne, ' ', p.nom_personne) AS nomAct,
+            f.titre_film,
+            f.annee_sortie_film
+        FROM
+            role r
+            INNER JOIN casting c ON c.id_role = r.id_role
+            INNER JOIN film f ON f.id_film = c.id_film
+            INNER JOIN acteur a ON c.id_acteur = a.id_acteur
+            INNER JOIN personne p ON p.id_personne = a.id_personne
+        WHERE
+            r.id_role = 1
+            ");
+        $requeteDetailRole>execute(["id" => $id]);
+    }
+
 }
 
