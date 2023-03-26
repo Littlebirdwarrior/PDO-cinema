4ead1e1c3adbdb341b2da4cc1e8aa2d416b5fc86
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-03-26 01:51:01 +0100

    listRoles.php et controller ok - bug sql corrigé

--- controller/RoleController.php
+++ controller/RoleController.php
@@ -6,22 +6,22 @@ use Model\Connect;
 class RoleController
 {
     public function listRoles(){
-        $pdo = Connect::seConnecter();
+        $pdo = Connect::seConnecter(); //ici requete faute, confusion id personne et id casting
         $requeteListRoles = $pdo->query("
             SELECT
                 r.id_role,
                 r.nom_role,
-                a.id_acteur,
+                c.id_acteur,
                 CONCAT(p.prenom_personne, ' ', p.nom_personne) AS nomAct,
                 f.id_film,
                 f.titre_film,
                 f.annee_sortie_film
             FROM
-                ROLE r
-                INNER JOIN casting c ON c.id_role = r.id_role
+                casting c
+                INNER JOIN role r ON c.id_role = r.id_role
                 INNER JOIN film f ON f.id_film = c.id_film
                 INNER JOIN acteur a ON c.id_acteur = a.id_acteur
-                INNER JOIN personne p ON p.id_personne = a.id_acteur
+                INNER JOIN personne p ON p.id_personne = a.id_personne
             ORDER BY nom_role
             ");
 
