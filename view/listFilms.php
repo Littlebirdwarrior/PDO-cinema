2269886ba59d06cd4cca2c6ca2f96c93ffd89446
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-03-28 09:12:51 +0200

    affichage des étoiles dans listFilm ok

--- view/listFilms.php
+++ view/listFilms.php
@@ -1,36 +1,39 @@
 <?php
-ob_start();//démarre session et créé tempon, capsule qui enregistre tout ce qui suit en string
+ob_start(); //démarre session et créé tempon, capsule qui enregistre tout ce qui suit en string
 ?>
 
-    <!--Compte des film-->
-    <p>Il y a <?=$requeteListFilms->rowCount()?> films</p>
-    <a href="index.php?action=addFilm">Ajouter un film</a>
+<!--Compte des film-->
+<p>Il y a <?= $requeteListFilms->rowCount() ?> films</p>
+<a href="index.php?action=addFilm">Ajouter un film</a>
 
-    <?php
+<?php
+$fetchFilms = $requeteListFilms->fetchAll();
 
-        $fetchFilms = $requeteListFilms->fetchAll();
-        //Fonction d'affichage des notes
-        function getNote($fetchFilms){
-            foreach ( $fetchFilms as $film) {    
-                //Récupère mon int du sql
-                $note = $film['note_film']; //int 
-                if($note !== null){
-                    $starsFilled = str_repeat('<i class="fa-solid fa-star"></i>', $note);
-                    $starsEmpty = str_repeat('<i class="fa-regular fa-star"></i>', (5 - $note));
-                    $stars = $starsFilled .''. $starsEmpty .'***';
-                    echo $stars;
-                } else {
-                    echo 'pas de note disponible';
-                }    
-            } 
-        }
-    ?>
 
-    <section>
-        <h2>Mes films</h2>
-        <div>
-            <table>
-                <thead>
+//Fonction d'affichage des notes
+foreach( $fetchFilms as $film) {
+    $note = $film['note_film']; //int(Récupère mon int du sql)
+}
+
+function getNote($note)
+{
+    if ($note !== null) {
+        $starsFilled = str_repeat('<i class="fa-solid fa-star"></i>', $note);
+        $starsEmpty = str_repeat('<i class="fa-regular fa-star"></i>', (5 - $note));
+        $stars = $starsFilled . '' . $starsEmpty;
+        return $stars;
+    } else {
+        return str_repeat('<i class="fa-regular fa-star"></i>', (5));
+    }
+}
+
+?>
+
+<section>
+    <h2>Mes films</h2>
+    <div>
+        <table>
+            <thead>
                 <tr>
                     <th>Titre</th>
                     <th>Année</th>
@@ -38,33 +41,33 @@ ob_start();//démarre session et créé tempon, capsule qui enregistre tout ce q
                     <th>Réalisateur</th>
                     <th>Notes</th>
                 </tr>
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 <?php
                 //boucle sur chaque films
-            
-                foreach ($fetchFilms as $film){
+
+                foreach ($fetchFilms as $film) {
 
                     //afficher les informations
                     echo '<tr>
-                                   <td><a href="http://localhost:8888/index.php?action=detailFilm&id='.$film["id_film"].'">'.$film['titre_film'].'</a></td>
-                                   <td>'.$film['annee_sortie_film'].'</td>
-                                   <td>'.$film['duree_film'].'</td>
-                                   <td><a href="http://localhost:8888/index.php?action=detailRealisateur&id="'.$film["id_realisateur"]. ' ">
-                                        </a>'.$film['nomReal'].'</a>
+                                   <td><a href="http://localhost:8888/index.php?action=detailFilm&id=' . $film["id_film"] . '">' . $film['titre_film'] . '</a></td>
+                                   <td>' . $film['annee_sortie_film'] . '</td>
+                                   <td>' . $film['duree_film'] . '</td>
+                                   <td><a href="http://localhost:8888/index.php?action=detailRealisateur&id="' . $film["id_realisateur"] . ' ">
+                                        </a>' . $film['nomReal'] . '</a>
                                     </td>
-                                   <td>'.getNote($fetchFilms).'</td>
+                                   <td>' . getNote($film['note_film']) . '</td>
                                </tr>
                         ';
                 }
                 ?>
-                </tbody>
-            </table>
-        </div>
-    </section>
+            </tbody>
+        </table>
+    </div>
+</section>
 
 <?php
 $titre = "liste des films";
-$titre_secondaire= "Liste des films";
-$contenu = ob_get_clean();//envois le tempons dans la session
+$titre_secondaire = "Liste des films";
+$contenu = ob_get_clean(); //envois le tempons dans la session
 require "view/template.php";
